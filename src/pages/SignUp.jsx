1dc02c594d8f4cbf982681ede9d682693c1f2bdf
Max Hughes <mhughes2011@gmail.com>
2022-03-04 18:46:00 -0800

    added firestore authentication to sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,6 +3,9 @@ import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config'
+
 function SignUp() {
   const [showPass, setShowPass] = useState(false)
   const [formData, setFormData] = useState({
@@ -23,6 +26,26 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -31,7 +54,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="text" className='nameInput' placeholder='Name' id='name' value={name} onChange={onChange} />
             <input type="email" className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
 
