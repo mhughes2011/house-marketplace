56b1de493415bb938d45acc472a8607cb5e4e70f
Max Hughes <mhughes2011@gmail.com>
2022-03-06 14:01:59 -0800

    implemented sign in authentication and started profile layout

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,8 @@ import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+
 function SignIn() {
   const [showPass, setShowPass] = useState(false)
   const [formData, setFormData] = useState({
@@ -22,6 +24,23 @@ function SignIn() {
     }))
   }
 
+  const onSubmit = async(e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+  
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+  
+      if(userCredentials.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -30,7 +49,7 @@ function SignIn() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="email" className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
 
             <div className="passwordInputDiv">
