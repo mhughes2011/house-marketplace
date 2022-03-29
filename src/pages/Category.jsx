6a5d0c1b6f9bc98e2c90848446d37cda36517959
Max Hughes <mhughes2011@gmail.com>
2022-03-28 17:25:28 -0700

    built out listing item for rentals component

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,6 +5,8 @@ import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
 
+import ListingItem from '../components/ListingItem'
+
 function Category() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
@@ -55,7 +57,7 @@ function Category() {
           <main>
             <ul className="categoryListings">
               {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem listing={listing.data} id={listing.id} key={listing.id} />
               ))}
             </ul>
           </main>
